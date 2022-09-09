b210aca5289adea5c3369386c9518cb88435df19
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-09-09 21:00:45 +0530

    Update and rename erfan4lx.py to manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -8,19 +8,8 @@ from telethon.tl.functions.channels import InviteToChannelRequest
 import time, os, sys, json
 
 wt = (
-    '''                                                
-  [[ye]]*   )                              (        
-[[re]]` )  /(    )        (      )         )\    )  
- [[ye]]( )(_))( /(   (    )\    (      (  ((_)( /(  
-[[re]](_(_()) )(_))  )\ )((_)   )\  '  )\  _  )(_)) 
-[[gr]]|_   _|((_)_  _(_/( (_) _((_))  ((_)| |((_)_  
-  [[gr]]| |  / _` || ' \))| || '  \()/ _ \| |/ _` | 
-  |_|  \__,_||_||_| |_||_|_|_| \___/|_|\__,_| 
-
-  M4n!fest0 Cyber Security Team [[re]][erfan4lx][[ye]] Thanks to my bro HACK4LX
-  
-  github.com/erfan4lx  
-  youtube.com/channel/UCHL7e6sD1eXIBIvjBYnXYEQ
+    '''                                              thanos-op & kaal-op
+                                                     Telegram-Member-adding
     '''
 )
 COLORS = {
@@ -42,6 +31,7 @@ elif clearType == 'c':
     clear = lambda:os.system('cls')
 else:
     print('Invalid input!!!')
+    os.system('pip install requests')
     sys.exit()
     
 if sys.version_info[0] < 3:
@@ -53,26 +43,137 @@ telet()
 time.sleep(1)
 clear()
 
-if os.path.isfile('erfan4lx_log.txt'):
-    with open('erfan4lx_log.txt', 'r') as r:
-        data = r.readlines()
-    api_id = data[0]
-    api_hash = data[1]
+while True:
+    clr()
+    banner()
+    print(lg+'[1] Add New Accounts'+n)
+    print(lg+'[2] Filter All Banned Accounts'+n)
+    print(lg+'[3] Delete specific accounts'+n)
+    print(lg+'[4] add member'+n)
+    print(lg+'[5] Update your Software'+n)
+    print(lg+'[6] Exit'+n)
+    a = int(input('\nEnter Your Choice: '))
+     if a == 1:
+        new_accs = []
+        with open('vars.txt', 'ab') as g:
+            number_to_add = int(input(f'\n{gr} [~] Enter number of accounts to add: {r}'))
+            for i in range(number_to_add):
+                phone_number = str(input(f'\n{ye} [~] Enter Phone Number: {r}'))
+                parsed_number = ''.join(phone_number.split())
+                pickle.dump([parsed_number], g)
+                new_accs.append(parsed_number)
+            print(f'\n{cy} [i] Saved all accounts in vars.txt')
+            clr()
+            print(f'\n{gr} [*] Logging in from new accounts\n')
+            for number in new_accs:
+                c = TelegramClient(f'sessions/{number}', 8088717 , '7d1e0295ee1c2628f1933e9ffd2d8b78')
+                c.start(number)
+                print(f'{ye}[+] 𝐋𝐨𝐠𝐢𝐧 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮L')
+                c.disconnect()
+            input(f'\n Press enter to goto main menu...')
 
-else:
-    api_id = input('Enter api_id: ')
-    api_hash = input('Enter api_hash: ')
-    with open('erfan4lx_log.txt', 'w') as a:
-        a.write(api_id + '\n' + api_hash)
+        g.close()
+    elif a == 2:
+        accounts = []
+        banned_accs = []
+        h = open('vars.txt', 'rb')
+        while True:
+            try:
+                accounts.append(pickle.load(h))
+            except EOFError:
+                break
+        h.close()
+        if len(accounts) == 0:
+            print(r+'[!] There are no accounts! Please add some and retry')
+            sleep(3)
+        else:
+            for account in accounts:
+                phone = str(account[0])
+                client = TelegramClient(f'sessions/{phone}', 8088717 , '7d1e0295ee1c2628f1933e9ffd2d8b78')
+                client.connect()
+                if not client.is_user_authorized():
+                    try:
+                        client.send_code_request(phone)
+                        #client.sign_in(phone, input('[+] Enter the code: '))
+                        print(f'{blue}[+] {phone} is not banned{n}')
+                    except PhoneNumberBannedError:
+                        print(r+str(phone) + ' is banned!'+n)
+                        banned_accs.append(account)
+            if len(banned_accs) == 0:
+                print(lg+'Congrats! No banned accounts')
+                input('\nPress enter to goto main menu...')
+            else:
+                for m in banned_accs:
+                    accounts.remove(m)
+                with open('vars.txt', 'wb') as k:
+                    for a in accounts:
+                        Phone = a[0]
+                        pickle.dump([Phone], k)
+                k.close()
+                print(lg+'[i] All banned accounts removed'+n)
+                input('\nPress enter to goto main menu...')
 
-client = TelegramClient('erfan4lx', api_id, api_hash)
+    elif a == 3:
+        accs = []
+        f = open('vars.txt', 'rb')
+        while True:
+            try:
+                accs.append(pickle.load(f))
+            except EOFError:
+                break
+        f.close()
+        i = 0
+        print(f'{ye}[i] Choose an account to delete\n')
+        for acc in accs:
+            print(f'{lg}[{i}] {acc[0]}{n}')
+            i += 1
+        index = int(input(f'\n{lg}[+] Enter a choice: {n}'))
+        phone = str(accs[index][0])
+        session_file = phone + '.session'
+        if os.name == 'nt':
+            os.system(f'del sessions\\{session_file}')
+        else:
+            os.system(f'rm sessions/{session_file}')
+        del accs[index]
+        f = open('vars.txt', 'wb')
+        for account in accs:
+            pickle.dump(account, f)
+        print(f'\n{lg}[+] Account Deleted{n}')
+        input(f'\nPress enter to goto main menu...')
+        f.close()
+    elif a == 5:
+        # thanks to github.com/kaal-op for the snippet below
+        print(f'\n{lg}[i] Checking for updates...')
+        try:
+            # https://raw.githubusercontent.com/kaal0408/Telegram-adder/main/version.txt
+            version = requests.get('https://raw.githubusercontent.com/kaal0408/Telegram-adder/main/version.txt')
+        except:
+            print(f'{r} You are not connected to the internet')
+            print(f'{r} Please connect to the internet and retry')
+            exit()
+        if float(version.text) > 0.9:
+            prompt = str(input(f'{lg}[~] Update available[Version {version.text}]. Download?[y/n]: {r}'))
+            if prompt == 'y' or prompt == 'yes' or prompt == 'Y':
+                print(f'{lg}[i] Downloading updates...')
+                if os.name == 'nt':
+                    os.system('del manjeet.py')
+                else:
+                    os.system('rm manjeet.py')
+                #os.system('del scraper.py')
+                os.system('curl -l -O https://raw.githubusercontent.com/kaal0408/Telegram-adder/main/manjeet.py')
+                print(f'{gr}[*] Updated to version: {version.text}')
+                input('Press enter to exit...')
+                exit()
+            else:
+                print(f'{lg}[!] Update aborted.')
+                input('Press enter to goto main menu...')
+        else:
+            print(f'{lg}[i] You is already up to date')
+            input('Press enter to goto main menu...')
+    elif a == 6:
+        clr()
+     
 
-async def main():
-    async def getmem():
-        clear()
-        print(colorText(wt))
-        print('')
-        print('')
         
         print(ye+'[+] Choose your channel to Add members.')
         a=0
@@ -85,7 +186,7 @@ async def main():
         my_participants_id = []
         for my_participant in my_participants:
             my_participants_id.append(my_participant.id)
-        with open('erfan4lx_members.txt', 'r') as r:
+        with open('manjeet_members.txt', 'r') as r:
             users = json.load(r)
         count = 1
         i = 0
